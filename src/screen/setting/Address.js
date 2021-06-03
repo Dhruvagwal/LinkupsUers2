c76b5e0c43199c004c6a92d5d924a541e7ef02b5
Dhruv Aggarwal <dhruvagwal.clus@gmail.com>
2021-06-04 04:09:26 +0530

    'Final_Commit'

---
+++ src/screen/setting/Address.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Address = () => {
+    return (
+        <View>
+            <Text>Dhruv</Text>
+        </View>
+    )
+}
+
+export default Address
+
+const styles = StyleSheet.create({})
